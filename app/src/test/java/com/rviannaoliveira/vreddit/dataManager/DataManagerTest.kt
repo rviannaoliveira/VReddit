8067e4b2e2a6a8dba566797bc4c3e2030c9bb4e7
Rodrigo Vianna <rodrigo.vianna.oliveira@gmail.com>
2017-10-21 19:58:38 -0200

    fix structure test

---
+++ app/src/test/java/com/rviannaoliveira/vreddit/dataManager/DataManagerTest.kt
@@ -0,0 +1,88 @@
+package vreddit.dataManager
+
+import com.nhaarman.mockito_kotlin.whenever
+import com.rviannaoliveira.vreddit.BuildConfig
+import com.rviannaoliveira.vreddit.data.DataManager
+import com.rviannaoliveira.vreddit.data.DataManagerInterface
+import com.rviannaoliveira.vreddit.data.api.RemoteDataSource
+import com.rviannaoliveira.vreddit.data.repository.CachedRepository
+import io.reactivex.Maybe
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+import org.robolectric.annotation.Config
+import vreddit.fake.RedditFakeFactory
+
+/**
+ * Criado por rodrigo on 21/10/17.
+ */
+@RunWith(MockitoJUnitRunner::class)
+@Config(constants = BuildConfig::class)
+class DataManagerTest {
+    @Mock private
+    lateinit var dataManager: DataManagerInterface
+
+    @Mock private
+    lateinit var apiDataResource: RemoteDataSource
+
+    @Mock private
+    lateinit var repositoryData: CachedRepository
+
+    @Before
+    fun setUp() {
+        dataManager = getDataManager()
+    }
+
+    @Test
+    fun getNewRedditsAndVerifyIfTheSameValue() {
+        val redditNewsDataResponse = RedditFakeFactory.fakeRedditNewsDataResponse
+        whenever(apiDataResource.getNewReddits()).thenReturn(Maybe.just(redditNewsDataResponse))
+        dataManager.getNewsReddits()
+                .test()
+                .assertValue(redditNewsDataResponse)
+    }
+
+    @Test
+    fun getNextPageNewRedditsAndVerifyIfTheSameValue() {
+        val redditNewsDataResponse = RedditFakeFactory.fakeRedditNewsDataResponse
+        val after = "wsd23"
+
+        whenever(apiDataResource.getNextPageNewReddit(Mockito.anyString())).thenReturn(Maybe.just(redditNewsDataResponse))
+        dataManager.getNextPageNewReddit(after)
+                .test()
+                .assertValue(redditNewsDataResponse)
+    }
+
+    @Test
+    fun getAllNewsLocalNewRedditsAndVerifyIfTheSameValue() {
+        val listRedditNewsData = mutableListOf(RedditFakeFactory.fakeRedditNewsData)
+
+        whenever(repositoryData.getAllNews()).thenReturn(Maybe.just(listRedditNewsData))
+        dataManager.getAllNewsLocal()
+                .test()
+                .assertValue(listRedditNewsData)
+    }
+
+    @Test
+    fun getAllCommentsNewAndVerifyIfTheSameValue() {
+        val redditCommentsDataNvl2ResponseIndex0 = RedditFakeFactory.getRedditCommentsDataNvl2Response("abcd")
+        val redditCommentsDataNvl2ResponseIndex1 = RedditFakeFactory.getRedditCommentsDataNvl2Response()
+        val listCallComments = listOf(redditCommentsDataNvl2ResponseIndex0, redditCommentsDataNvl2ResponseIndex1)
+
+        whenever(apiDataResource.getAllCommentsNew(Mockito.anyString())).thenReturn(Maybe.just(listCallComments))
+
+        dataManager.getAllCommentsNew(Mockito.anyString())
+                .test()
+                .assertValue({ (reddiCommentData) ->
+                    reddiCommentData.id == redditCommentsDataNvl2ResponseIndex0.data!!.children[0].data!!.id
+                })
+    }
+
+
+    private fun getDataManager(): DataManager {
+        return DataManager(apiDataResource, repositoryData)
+    }
+}
---
+++ app/src/test/java/com/rviannaoliveira/vreddit/presenter/ListingPresenterTest.kt
@@ -0,0 +1,68 @@
+package vreddit.presenter
+
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import com.rviannaoliveira.vreddit.BuildConfig
+import com.rviannaoliveira.vreddit.data.DataManagerInterface
+import com.rviannaoliveira.vreddit.listing.ListingInterface
+import com.rviannaoliveira.vreddit.listing.ListingPresenterImpl
+import com.rviannaoliveira.vreddit.modal.RedditNewsData
+import com.rviannaoliveira.vreddit.modal.RedditNewsDataResponse
+import io.reactivex.Maybe
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+import org.robolectric.annotation.Config
+
+/**
+ * Criado por rodrigo on 21/10/17.
+ */
+@RunWith(MockitoJUnitRunner::class)
+@Config(constants = BuildConfig::class)
+class ListingPresenterTest {
+    @Mock private
+    lateinit var listingView: ListingInterface.ListingView
+
+    @Mock private
+    lateinit var dataManager: DataManagerInterface
+
+    @Mock private
+    lateinit var redditNewsDataResponse: RedditNewsDataResponse
+
+    private var listingPresenter: ListingInterface.ListingPresenter? = null
+
+    @Before
+    fun setUp() {
+        listingPresenter = getListingPresenter()
+    }
+
+    @Test
+    fun loading_News_Posts_In_The_ListingView() {
+        val hasInternet = true
+
+        whenever(dataManager.getNewsReddits()).thenReturn(Maybe.just(redditNewsDataResponse))
+        listingPresenter?.onViewCreated(hasInternet)
+
+        verify(this.listingView).showProgressBar()
+        verify(this.listingView).saveNextPage(Mockito.anyString())
+        verify(this.listingView).loadNewReddits(ArgumentMatchers.anyList<RedditNewsData>())
+        verify(this.listingView).hideProgressBar()
+    }
+
+    @Test
+    fun loading_Next_Page_In_The_ListingView() {
+        whenever(dataManager.getNextPageNewReddit(Mockito.anyString())).thenReturn(Maybe.just(redditNewsDataResponse))
+        listingPresenter?.loadNextPageNewRedditsList(Mockito.anyString())
+
+        verify(this.listingView).saveNextPage(Mockito.anyString())
+        verify(this.listingView).loadNewReddits(ArgumentMatchers.anyList<RedditNewsData>())
+    }
+
+    private fun getListingPresenter(): ListingPresenterImpl {
+        return ListingPresenterImpl(listingView, dataManager)
+    }
+}
